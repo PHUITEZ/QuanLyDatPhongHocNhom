55799739761e5c2120f7de5d0d3b488d93c4cccd
DoNhuPhu <23010553@st.phenikaa-uni.edu.vn>
2026-06-05 10:41:37 +0700

    tôi sửa file

--- src/model/User.java
+++ src/model/User.java
@@ -1,4 +1,23 @@
 package model;
+import java.io.Serializable;
 
-public class User {
+public abstract class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String fullName;
+    private String phoneNumber;
+    private String email;
+
+    public User() {}
+    public User(String fullName, String phoneNumber, String email) {
+        this.fullName = fullName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getFullName() { return fullName; }
+    public void setFullName(String fullName) { this.fullName = fullName; }
+    public String getPhoneNumber() { return phoneNumber; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 }
