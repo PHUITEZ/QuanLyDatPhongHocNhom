6f0b98897611fd591ebdf026e2d1ddb5bf234ab2
Tran Hoang Manh <tranmanh7962@gmail.com>
2026-06-06 21:57:29 +0700

    sua phan model

--- src/model/TimeSlot.java
+++ src/model/TimeSlot.java
@@ -1,4 +1,32 @@
 package model;
 
+import java.time.LocalDateTime;
+
 public class TimeSlot {
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    public TimeSlot() {
+    }
+
+    public TimeSlot(LocalDateTime startTime, LocalDateTime endTime) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
 }
