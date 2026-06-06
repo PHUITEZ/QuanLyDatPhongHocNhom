5cfed6cd5ec0e2e780a2c8f8a57391346db71d56
Tran Hoang Manh <tranmanh7962@gmail.com>
2026-06-06 21:46:15 +0700

    sua phan model

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,4 +1,60 @@
 package model;
 
 public class Student {
+    private String studentId;
+    private String fullName;
+    private String phoneNumber;
+    private String className;
+    private String email;
+
+    public Student() {
+    }
+
+    public Student(String studentId, String fullName, String phoneNumber, String className, String email) {
+        this.studentId = studentId;
+        this.fullName = fullName;
+        this.phoneNumber = phoneNumber;
+        this.className = className;
+        this.email = email;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
