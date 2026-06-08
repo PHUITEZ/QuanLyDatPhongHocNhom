db212450889170598718314fa54ea61c53c3ae6a
DoNhuPhu <23010553@st.phenikaa-uni.edu.vn>
2026-06-08 20:19:10 +0700

    Tôi sửa lại code phần timeslot

--- src/model/TimeSlot.java
+++ src/model/TimeSlot.java
@@ -1,4 +1,33 @@
 package model;
 
-public class TimeSlot {
+import java.io.Serializable;
+
+// Lớp này không kế thừa ai, chỉ dùng để gom nhóm thời gian
+public class TimeSlot implements Serializable {
+    // Đóng gói dữ liệu (Encapsulation)
+    private String startTime;
+    private String endTime;
+
+    // Constructor có tham số
+    public TimeSlot(String startTime, String endTime) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    // Các hàm getter/setter để truy xuất dữ liệu an toàn
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
 }
