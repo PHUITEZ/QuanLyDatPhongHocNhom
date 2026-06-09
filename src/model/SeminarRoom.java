d61044d1ebc21fc075318d86b32cd4ef58750a33
PHUITEZ <Phun7904@gmail.com>
2026-06-09 07:20:55 +0700

    Prepare room module for member implementation

--- src/model/SeminarRoom.java
+++ src/model/SeminarRoom.java
@@ -1,43 +1,4 @@
 package model;
 
 public class SeminarRoom extends Room {
-
-    private boolean soundSystem;
-
-    public SeminarRoom(String roomId,
-                       String roomName,
-                       int capacity,
-                       boolean soundSystem) {
-
-        super(roomId, roomName, capacity);
-        this.soundSystem = soundSystem;
-    }
-
-    public boolean isSoundSystem() {
-        return soundSystem;
-    }
-
-    public void setSoundSystem(boolean soundSystem) {
-        this.soundSystem = soundSystem;
-    }
-
-    @Override
-    public double calculateFee(int hours) {
-        return hours * 30000;
-    }
-
-    @Override
-    public void displayInfo() {
-
-        System.out.println(
-                "Seminar Room | ID: "
-                        + roomId
-                        + " | Name: "
-                        + roomName
-                        + " | Capacity: "
-                        + capacity
-                        + " | Sound System: "
-                        + soundSystem
-        );
-    }
 }
