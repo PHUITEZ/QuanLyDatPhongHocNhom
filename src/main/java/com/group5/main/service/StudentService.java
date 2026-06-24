9c5aa1e26e5ded44917a3d110cb39b45e73bb49a
PHUITEZ <Phun7904@gmail.com>
2026-06-24 15:22:19 +0700

    sửa lại cấu trúc code

--- src/main/java/com/group5/main/service/StudentService.java
+++ src/main/java/com/group5/main/service/StudentService.java
@@ -2,10 +2,12 @@ package com.group5.main.service;
 
 import com.group5.main.exception.BookingException;
 import com.group5.main.model.Student;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class StudentService {
 
     private List<Student> students = new ArrayList<>();
