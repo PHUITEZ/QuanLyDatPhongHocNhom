8d164c1918908deb40f43f09f606cdf8ff7a1cc4
PHUITEZ <Phun7904@gmail.com>
2026-06-24 15:22:01 +0700

    sửa lại cấu trúc code

--- src/main/java/com/group5/main/controller/RoomController.java
+++ src/main/java/com/group5/main/controller/RoomController.java
@@ -1,13 +1,23 @@
 package com.group5.main.controller;
 
+import com.group5.main.model.Room;
+import com.group5.main.service.RoomService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class RoomController {
 
+    private final RoomService roomService;
+
+    public RoomController(RoomService roomService) {
+        this.roomService = roomService;
+    }
+
     @GetMapping("/rooms")
-    public String test() {
-        return "Rooms API OK";
+    public List<Room> getAllRooms() {
+        return roomService.getAllRooms();
     }
 }
