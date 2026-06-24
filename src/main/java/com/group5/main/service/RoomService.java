c56f50866d4f98acea334350a9b5cd98b8edda72
PHUITEZ <Phun7904@gmail.com>
2026-06-24 12:50:38 +0700

    sửa lại cấu trúc code

--- src/main/java/com/group5/main/service/RoomService.java
+++ src/main/java/com/group5/main/service/RoomService.java
@@ -1,9 +1,14 @@
 package com.group5.main.service;
 
+import com.group5.main.exception.BookingException;
+import com.group5.main.model.Room;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
 public class RoomService {
+
     private List<Room> rooms = new ArrayList<>();
 
     public void addRoom(Room room) {
@@ -15,15 +20,22 @@ public class RoomService {
     }
 
     public Room findRoomById(String roomId) throws BookingException {
+
         return rooms.stream()
                 .filter(r -> r.getRoomId().equalsIgnoreCase(roomId))
                 .findFirst()
-                .orElseThrow(() -> new BookingException("Loi: Phong co ma " + roomId + " khong ton tai!"));
+                .orElseThrow(() ->
+                        new BookingException(
+                                "Loi: Phong co ma " + roomId + " khong ton tai!"
+                        ));
     }
 
     public List<Room> findRoomsByType(String roomType) {
+
         return rooms.stream()
-                .filter(r -> r.getClass().getSimpleName().equalsIgnoreCase(roomType))
+                .filter(r ->
+                        r.getClass().getSimpleName()
+                                .equalsIgnoreCase(roomType))
                 .collect(Collectors.toList());
     }
 }
