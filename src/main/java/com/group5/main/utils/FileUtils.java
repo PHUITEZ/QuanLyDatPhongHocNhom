b28d37a207268fb728e84e7e45705953c1ce9302
PHUITEZ <Phun7904@gmail.com>
2026-06-24 12:51:34 +0700

    sửa lại cấu trúc code

--- src/main/java/com/group5/main/utils/FileUtils.java
+++ src/main/java/com/group5/main/utils/FileUtils.java
@@ -1,19 +1,24 @@
 package com.group5.main.utils;
 
+import com.group5.main.model.Room;
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
+
 public class FileUtils {
 
-    public static void saveRooms(List<Room> rooms,
-                                 String fileName) {
+    public static void saveRooms(List<Room> rooms, String fileName) {
 
         try (FileWriter writer = new FileWriter(fileName)) {
 
             for (Room room : rooms) {
 
                 writer.write(
-                        room.getRoomId() + "," + room.getRoomName() + "," + room.getMaxCapacity() + "\n");
+                        room.getRoomId() + ","
+                                + room.getRoomName() + ","
+                                + room.getMaxCapacity()
+                                + "\n");
             }
 
             System.out.println("Luu file thanh cong!");
