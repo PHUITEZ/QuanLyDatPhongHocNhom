88bf02f3f5a88cab684954dd97c2268161542464
PHUITEZ <Phun7904@gmail.com>
2026-06-24 15:21:25 +0700

    sửa lại cấu trúc code

---
+++ src/main/java/com/group5/main/controller/BookingController.java
@@ -0,0 +1,23 @@
+package com.group5.main.controller;
+
+import com.group5.main.model.Booking;
+import com.group5.main.service.BookingService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class BookingController {
+
+    private final BookingService bookingService;
+
+    public BookingController(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
+
+    @GetMapping("/bookings")
+    public List<Booking> getAllBookings() {
+        return bookingService.getBookings();
+    }
+}
