47cc55a18e715b42b227ada88b869b47a785f27d
PHUITEZ <Phun7904@gmail.com>
2026-06-24 12:56:27 +0700

    sửa lại cấu trúc code

--- src/main/java/com/group5/main/repository/BookingRepository.java
+++ src/main/java/com/group5/main/repository/BookingRepository.java
@@ -7,35 +7,44 @@ import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.List;
+
 public class BookingRepository {
 
-    public void saveBookings(ArrayList<Booking> dsDatPhong) {
+    public void saveBookings(List<Booking> dsDatPhong) {
+
         try {
             FileOutputStream fos = new FileOutputStream("bookings.dat");
             ObjectOutputStream oos = new ObjectOutputStream(fos);
+
             oos.writeObject(dsDatPhong);
 
             oos.close();
             fos.close();
+
         } catch (Exception e) {
             System.out.println("Loi khi ghi file bookings.dat: " + e.getMessage());
         }
     }
 
-    public ArrayList<Booking> loadBookings() {
-        ArrayList<Booking> dsDatPhong = new ArrayList<>();
+    public List<Booking> loadBookings() {
+
+        List<Booking> dsDatPhong = new ArrayList<>();
+
         try {
+
             FileInputStream fis = new FileInputStream("bookings.dat");
             ObjectInputStream ois = new ObjectInputStream(fis);
 
-            // Ép kiểu
-            dsDatPhong = (ArrayList<Booking>) ois.readObject();
+            dsDatPhong = (List<Booking>) ois.readObject();
 
             ois.close();
             fis.close();
+
         } catch (Exception e) {
-            // Sinh viên thường để trống catch nếu chỉ muốn khởi tạo file mới
+            // Nếu file chưa tồn tại thì trả về danh sách rỗng
         }
+
         return dsDatPhong;
     }
 }
