979527b776318b2b4b3c88b9cc5105f12354cba8
PHUITEZ <Phun7904@gmail.com>
2026-06-25 23:41:57 +0700

    sửa lại cấu trúc code

--- src/main/java/com/group5/main/service/StudentService.java
+++ src/main/java/com/group5/main/service/StudentService.java
@@ -2,32 +2,48 @@ package com.group5.main.service;
 
 import com.group5.main.exception.BookingException;
 import com.group5.main.model.Student;
+import com.group5.main.repository.StudentRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class StudentService {
 
-    private List<Student> students = new ArrayList<>();
+    private List<Student> students;
+
+    private final StudentRepository studentRepository =
+            new StudentRepository();
+
+    public StudentService() {
+
+        this.students =
+                studentRepository.loadStudents();
+    }
 
     public void addStudent(Student student) {
+
         students.add(student);
+
+        studentRepository.saveStudents(students);
     }
 
     public List<Student> getAllStudents() {
         return students;
     }
 
-    public Student findStudentById(String studentId) throws BookingException {
+    public Student findStudentById(String studentId)
+            throws BookingException {
 
         return students.stream()
-                .filter(s -> s.getStudentId().equalsIgnoreCase(studentId))
+                .filter(s ->
+                        s.getStudentId()
+                                .equalsIgnoreCase(studentId))
                 .findFirst()
                 .orElseThrow(() ->
                         new BookingException(
-                                "Loi: Sinh vien co ma " + studentId + " khong ton tai!"
-                        ));
+                                "Loi: Sinh vien co ma "
+                                        + studentId
+                                        + " khong ton tai!"));
     }
 }
