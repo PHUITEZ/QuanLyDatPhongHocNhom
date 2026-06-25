012f592e03434721ec635a95690044931ee086a7
PHUITEZ <Phun7904@gmail.com>
2026-06-25 23:09:30 +0700

    sửa lại cấu trúc code

--- src/main/java/com/group5/main/model/NormalRoom.java
+++ src/main/java/com/group5/main/model/NormalRoom.java
@@ -1,7 +1,23 @@
 package com.group5.main.model;
 
 public class NormalRoom extends Room {
-    public NormalRoom(String roomId, String roomName, int floor, int maxCapacity, String status) {
-        super(roomId, roomName, floor, maxCapacity, status, hours -> 0.0);
+
+    public NormalRoom() {
+        super();
+        setFeePolicy(hours -> 0.0);
+    }
+
+    public NormalRoom(String roomId,
+                      String roomName,
+                      int floor,
+                      int maxCapacity,
+                      String status) {
+
+        super(roomId,
+                roomName,
+                floor,
+                maxCapacity,
+                status,
+                hours -> 0.0);
     }
 }
