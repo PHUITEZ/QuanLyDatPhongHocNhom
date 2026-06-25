942cd51ce0d2757de5529f03f8bf81bf45c3eab0
PHUITEZ <Phun7904@gmail.com>
2026-06-25 23:09:45 +0700

    sửa lại cấu trúc code

--- src/main/java/com/group5/main/model/TimeSlot.java
+++ src/main/java/com/group5/main/model/TimeSlot.java
@@ -1,15 +1,20 @@
 package com.group5.main.model;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
-public class TimeSlot {
+public class TimeSlot implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
     private LocalDateTime startTime;
     private LocalDateTime endTime;
 
     public TimeSlot() {
     }
 
-    public TimeSlot(LocalDateTime startTime, LocalDateTime endTime) {
+    public TimeSlot(LocalDateTime startTime,
+                    LocalDateTime endTime) {
         this.startTime = startTime;
         this.endTime = endTime;
     }
